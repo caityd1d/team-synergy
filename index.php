4775ec159d682f0617a4fe5d67086947399c5ced
rushita1 <rushitakatiria@gmail.com>
2014-06-03 10:55:19 -0700

    html page

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	
+	<script src="main.js"></script>
+
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+	<div id="strapline"></div>
+
+	 <nav>
+      <div id="menubar">
+          <ul id="nav">
+            <li class="current"><a href="index.html">Home</a></li>
+            <li><a href="ourwork.html">Our Work</a></li>
+            <li><a href="testimonials.html">Testimonials</a></li>
+          
+            <li><a href="contact.html">Contact Us</a></li>
+          </ul>
+        </div>
+      </nav>
+
+
+</header>
+
+<main>
+	
+
+
+</main>
+
+<footer>
+	
+
+</footer>
+
+</body>
+</html>
