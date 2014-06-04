5b1d52e79b4bcdc8df46a8b4a85f29737074b5d7
Stefan Swiat <stefanswiat@Stefans-MacBook-Air.local>
2014-06-04 09:41:43 -0700

    added beginning of survey page to git

---
+++ survey.php
@@ -0,0 +1,30 @@
+<html>
+    <body>
+        <header></header>
+            <form action="" method="POST">
+
+            Work/Life Balance: <input type="range" name="balance"><br>
+            Salary: <input type="range" name="last_name"> <br>
+            Benfits: <input type="range" name="benefits"><br>
+            Advancement Opportunties: <input type="range" name="advancement"><br>
+            Fairness/Equality: <input type="range" name="equality"><br>
+            Quality of Leadership: <input type="range" name="leadership"><br>
+            Loyalty: <input type="range" name="loyalty"><br>
+            Morale: <input type="range" name="morale"><br>
+            Communication: <input type="range" name="communication"><br>
+
+            </form>
+
+        </body>
+</html>
+
+
+<?php
+
+
+
+
+
+
+
+?>
