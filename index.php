4e5a984de13f0cb809bf66880cd4902418dba66c
rushita1 <rushitakatiria@gmail.com>
2014-06-04 16:18:12 -0700

    improved landing page

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-	
-	<script src="main.js"></script>
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  
+  <script src="main.js"></script>
 
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <header>
-	<div id="strapline"></div>
+  
 
-	 <nav>
+   <nav>
       <div id="menubar">
           <ul id="nav">
             <li class="current"><a href="index.html">Home</a></li>
-            <li><a href="ourwork.html">About Us</a></li>
+            <li><a href="ourwork.html">Our Work</a></li>
             <li><a href="testimonials.html">Testimonials</a></li>
           
             <li><a href="contact.html">Contact Us</a></li>
@@ -26,14 +26,76 @@
 
 </header>
 
-<main>
-	
-<a href="survey.php"><h1>Go To The Survey</h1></a>
+<aside>
+  <div class="aside">
 
+  <ul class="slider">
+    <li>
+        <input type="radio" id="slide1" name="slide" checked>
+        <label for="slide1"></label>
+        <img src="1.jpg">
+    </li>
+    <li>
+        <input type="radio" id="slide2" name="slide" checked>
+        <label for="slide2"></label>
+        <img src="12.jpg">
+    </li>
+    <li>
+        <input type="radio" id="slide3" name="slide" checked>
+        <label for="slide3"></label>
+        <img src="1234.jpg">
+    </li>
+    <li>
+        <input type="radio" id="slide4" name="slide" checked>
+        <label for="slide4"></label>
+        <img src="12345.jpg">
+    </li>
+</ul>
+  
+
+</div>
+  
+
+
+
+
+
+</aside>
+
+
+<main>
+  
+<div class="main">
+  <div id="project">
+
+
+            
+              <div id="prospects projDiv">
+                <h2>Prospects</h2>
+                
+              </div>
+            
+
+            
+              <div id="employers projDiv">
+                <h2>Employers</h2>
+                
+              </div>
+            
+
+              <div id="employee projDiv">
+                <h2>Employee</h2>
+                </div>
+
+  </div>
+</div>
+  
 </main>
 
+
+
 <footer>
-	
+  
 
 </footer>
 
