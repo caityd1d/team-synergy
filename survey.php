d6aa2487cae044ab3d4b13d0b62fb562c994373c
Stefan Swiat <stefanswiat@stefans-air.apollosrc.root>
2014-06-04 14:56:57 -0700

    fixed slider values

--- survey.php
+++ survey.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-
 
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
@@ -25,44 +23,54 @@
             <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
             <script src="main.js"></script>
         </header>
-            <form action="" method="POST">
+            <form action="survey.php" method="POST">
 
             
             <fieldset>
-<legend>Lex Luthor’s Earthquake Machine</legend>
-<label for=richter>Richter Scale</label>
-<!-- <input id="richterscale" type="range" min="0" max="10" step="1" /> -->
-<input type=range min=0 max=10 value=0 id='richter' step=1 list=richterscale>
-<datalist id=richterscale>
-<option>0</option>
-<option>1</option>
-<option>2</option>
-<option>3</option>
-<option>4</option>
-<option>5</option>
-<option>6</option>
-<option>7</option>
-<option>8</option>
-<option>9</option>
-<option>10</option>
-</datalist>
-            Work/Life Balance: <input type="range" name="balance" class="priorities"><br>
-            Salary: <input type="range" name="last_name" class="priorities"> <br>
-            Benefits: <input type="range" name="benefits" class="priorities"><br>
-            Advancement Opportunities: <input type="range" name="advancement" class="priorities"><br>
-            Fairness/Equality: <input type="range" name="equality" class="priorities"><br>
-            Quality of Leadership: <input type="range" name="leadership" class="priorities"><br>
-            Loyalty: <input type="range" name="loyalty" class="priorities"><br>
-            Morale: <input type="range" name="morale" class="priorities"><br>
-            Communication: <input type="range" name="communication" class="priorities"><br>
+                <legend>Rate Your Employer</legend>
+                <label for='balance'>Work/Life Balance</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="balance" class="ranger"><p class="count" style="display: inline"></p>
+                <datalist id='balance'>
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                </datalist>
+                <br>        
+                <label for='salary'>Salary</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="salary" class="ranger"><p class="count" style="display: inline"></p>
+                <br>        
+                <label for='benefits'>Benefits</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="benefits" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='advancement'>Advancement Opportunities</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="advancement" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='equality'>Fairness/Equality</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="equality" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='leadership'>Quality of Leadership</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="leadership" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='loyalty'>Loyalty</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="loyalty" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='morale'>Morale</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="morale" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <label for='communication'>Communication</label>
+                <input type=range min=0 max=10 value=0 step=1 list='balance' name="communication" class="ranger"><p class="count" style="display: inline"></p>
+                <br>
+                <button type="submit">Submit</button>
+            </fieldset>
+        </form>
+    </body>
+</html>
 
-=======
-
-
-
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> aa55238467989d7c1bd2e127256c894da96c6392
