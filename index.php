13a270cdc13b267ddb0721793702d5a065332540
rushita1 <rushitakatiria@gmail.com>
2014-06-06 15:50:53 -0700

    jquery slider

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-  
-  <script src="main.js"></script>
 
+  <link rel="stylesheet" href="nivo-slider.css" media="screen">
   <link rel="stylesheet" href="style.css">
+ 
 </head>
+
 <body>
+
+
+
+
 <header>
   
 
    <nav>
       <div id="menubar">
           <ul id="nav">
-            <li class="current"><a href="index.html">Home</a></li>
-            <li><a href="ourwork.html">Our Work</a></li>
-            <li><a href="testimonials.html">Testimonials</a></li>
-          
-            <li><a href="contact.html">Contact Us</a></li>
+            <li class="current"><a href="index.html" class="link">Home</a></li>
+            <li><a href="ourwork.html" class="link">Companies</a></li>
+            <li><a href="contact.html" class="link">Contact Us</a></li>
+            <li><a href="newuser.php" class="login">Log In</a></li>
           </ul>
         </div>
       </nav>
@@ -28,39 +31,26 @@
 
 
 <aside>
-  <div class="aside">
+ 
 
-  <ul class="slider">
-    <li>
-        <input type="radio" id="slide1" name="slide" checked>
-        <label for="slide1"></label>
-        <img src="1234.jpg">
-    </li>
-    <li>
-        <input type="radio" id="slide2" name="slide" checked>
-        <label for="slide2"></label>
-        <img src="12.jpg">
-    </li>
-    <li>
-        <input type="radio" id="slide3" name="slide" checked>
-        <label for="slide3"></label>
-        <img src="1.jpg">
-    </li>
-    <li>
-        <input type="radio" id="slide4" name="slide" checked>
-        <label for="slide4"></label>
-        <img src="12345.jpg">
-    </li>
-</ul>
+<div class="slider-wrapper futurico-theme">
+ 
+    <div id="slider" class="nivoSlider">
   
-
+        <img src="slide1.png">
+ 
+        <img src="slide2.png">
+ 
+        <img src="slide5.png">
+ 
+        <img src="slide4.png">
+ 
+    </div>
+ 
 </div>
+
+
   
-
-
-
-
-
 </aside>
 
 
@@ -102,5 +92,12 @@
 
 </footer>
 
+
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+ 
+  <script src="jquery.nivo.slider.pack.js"></script>
+  <script src="slide.js"></script>
 </body>
 </html>
+
+ 
