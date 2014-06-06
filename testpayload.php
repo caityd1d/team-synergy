ea8a29daf342606c504e94b73fa2676f3303e858
Stefan Swiat <stefanswiat@stefans-air.apollosrc.root>
2014-06-06 15:21:09 -0700

    search implemented

--- testpayload.php
+++ testpayload.php
@@ -6,22 +6,31 @@ include 'db.php';
 include 'payload.php';
 
 $db = new DB;
-$sql = "SELECT * FROM Companies";
+$search = $_GET['search'];
+print_r($search);
+
+if ($search===''){
+    $sql = "SELECT * FROM Companies";
+} else {
+    $sql = "SELECT * FROM Companies WHERE Name LIKE '%$search%'";
+
+}
+
 $results = $db->execute($sql);
 
 Payload::$values = [];
+
 while($row = $results->fetch_assoc()){
     $a = [];
     $a['Name'] = $row['Name'];
     $a['Industry'] = $row['Industry'];
     $a['Website'] = $row['Website'];
-    
+    $a['Logo'] = $row['Logo'];
     array_push(Payload::$values, $a);
-}
-
-
+ }   
     $payload = json_encode(Payload::$values);
     print_r(Payload::$values);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,6 +42,10 @@ while($row = $results->fetch_assoc()){
     <script src="render.js"></script>
 </head>
 <body>
+     <form action="testpayload.php" method="GET">
+        <input type="text" name="search" placeholder="Find a company rating" size="75"><br>
+        <button type="submit">Submit</button>
+    </form>
     <ul class="listing">
         
     </ul>
