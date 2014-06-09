2a09a279da77afce70401af4351259f140bfd259
rushita1 <rushitakatiria@gmail.com>
2014-06-09 11:21:26 -0700

    create a temlate

---
+++ template.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <link rel="stylesheet" href="nivo-slider.css" media="screen">
+  <link rel="stylesheet" href="template.css"> 
+ 
+</head>
+
+<body>
+
+<header>
+
+
+	
+</header>
+
+<main>
+	<div class="logo"></div>
+
+	<div class="desc"></div>
+		<a href="" class="link"></a>
+
+
+
+</main>
+
+<section>
+ratings go here
+</section>
+
+
+
+
+
+
+
+</body>
