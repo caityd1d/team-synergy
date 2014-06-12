a2bd112075614a07ceac33cb37a5c06b143895a6
caityd1d <caitlin.lindsey@dhcp-10-158-194-224.apollosrc.root>
2014-06-12 15:19:59 -0700

    See header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+            <a href="index.html" class="link">Home</a>
+            <a href="ourwork.html" class="link">Companies</a>
+            <a href="contact.html" class="link">Contact Us</a>
+            <a href="newuser.php" class="login">Log In</a>
+  </nav>
+</header>
