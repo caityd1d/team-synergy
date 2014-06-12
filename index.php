18c509fde51c1debb31ed6e1e07c927f865c7b36
caityd1d <caitlin.lindsey@dhcp-10-158-194-224.apollosrc.root>
2014-06-12 15:12:27 -0700

    modified CSS, changed slider images

--- index.php
+++ index.php
@@ -34,9 +34,11 @@
  
         <img src="img/slide2.jpg" alt="">
  
-        <img src="img/slide5.jpg" alt="">
+        <img src="img/slide3.jpg" alt="">
  
-        <img src="img/slide4.png" alt="">
+        <img src="img/slide4.jpg" alt="">
+
+        <img src="img/slide5.jpg" alt="">
  
     </div>
  
