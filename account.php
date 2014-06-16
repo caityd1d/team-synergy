79d27d998f9a12247d30911752c3fb38afa0dedd
Stefan Swiat <stefanswiat@stefans-air.apollosrc.root>
2014-06-16 11:53:30 -0700

    allowed for ratings to appear

--- account.php
+++ account.php
@@ -12,3 +12,20 @@ if (!is_numeric($_SESSION['user_id'])) {
 echo 'You\'re logged in ' . $_SESSION['email'];
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+
+    <?php include 'header.php';?>
+    
+</body>
+
+
+</html>
