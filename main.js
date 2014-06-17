cff5ca7a2005cdf583e8943e1e059d4c844fc157
Jon Pfeiffer <kryowyrm@gmail.com>
2014-06-17 13:31:22 -0700

    fixed logout button issues

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ $(function(){
         var value = $(this).val();
         $(this).next().text(value);
     });
-
-<<<<<<< HEAD
-    $('button').on('click', '.signOut', function(){
-        window.location = "logout.php";
-=======
-    $('body').on('click', '.signOut', function(){
+    
+    $('.signOut').click(function(){
        
-        window.location = '/Team-Synergy-Team/logout.php';
+        window.location = 'logout.php';
 
->>>>>>> 11710586de0ebdfe42d172d1b845f9d468acf29b
+
     });
 
 });
