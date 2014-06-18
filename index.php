c9795f6e65a5e5d62256d7ed1ff57613e75c60c8
rushita1 <rushitakatiria@gmail.com>
2014-06-18 15:03:55 -0700

    button

--- index.php
+++ index.php
@@ -64,6 +64,7 @@ include 'initialize.php';
             
               <div class="employers">
                 <h2>Employers</h2>
+                <button class="btn" onclick="window.location.href='companies.php'">see you ratings</button>
                 
               </div>
             
